872587861ed1808ec61980db3234f7f3b5281f89
jalssonn <segalangnya@gmail.com>
2024-05-06 12:08:05 +0700

    seccond commit

--- src/api/middlewares/authentication-middleware.js
+++ src/api/middlewares/authentication-middleware.js
@@ -2,21 +2,34 @@ const passport = require('passport');
 const passportJWT = require('passport-jwt');
 
 const config = require('../../core/config');
-const { User } = require('../../models');
+const { User, Product } = require('../../models');
 
-// Authenticate user based on the JWT token
+// General strategy for both user and product
 passport.use(
-  'user',
   new passportJWT.Strategy(
     {
       jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderWithScheme('jwt'),
       secretOrKey: config.secret.jwt,
     },
     async (payload, done) => {
-      const user = await User.findOne({ id: payload.user_id });
-      return user ? done(null, user) : done(null, false);
+      try {
+        const user = await User.findOne({ id: payload.user_id });
+        if (!user) return done(null, false);
+
+        // Optionally check if the user has access to products
+        if (payload.access && payload.access.includes('products')) {
+          const product = await Product.findOne({ id: payload.product_id });
+          if (!product) return done(null, false);
+        }
+
+        return done(null, user); // User is authenticated and optionally checked for product access
+      } catch (error) {
+        return done(error, false);
+      }
     }
   )
 );
 
-module.exports = passport.authenticate('user', { session: false });
+module.exports = {
+  authenticate: passport.authenticate('jwt', { session: false }),
+};
